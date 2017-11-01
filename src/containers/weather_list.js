91d8a9f6b3bcc6fcfb3b94ca0274c97603fd2ac8
Piotr <piotr.szablewski@vp.pl>
2017-11-01 19:10:42 +0100

    adding the sparkline lib and loading to the app with consuming the data

---
+++ src/containers/weather_list.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Sparklines, SparklinesLine } from 'react-sparklines'
+
+class WeatherList extends Component {
+    renderWeather( cityData ) {
+        let temps = cityData.list.map( listItem => {
+            let kelvinTemp = listItem.main.temp;
+            return ( kelvinTemp - 270 ).toFixed( 0 )
+        } );
+        console.log( temps );
+        return (
+            <tr key={ cityData.city.id }>
+                <td>{ cityData.city.name }</td>
+                <td>
+                    <Sparklines height={ 120 } width={ 180 } data={ temps }>
+                        <SparklinesLine color="red"/>
+                    </Sparklines>
+                </td>
+            </tr>
+        )
+    }
+
+    render() {
+
+        return (
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>City</th>
+                    <th>Temperature</th>
+                    <th>Pressure</th>
+                    <th>Humidity</th>
+                </tr>
+                </thead>
+                <tbody>
+                { this.props.weather.map( this.renderWeather ) }
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps( { weather } ) {
+    return { weather };
+}
+
+export default connect( mapStateToProps )( WeatherList );
